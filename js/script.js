37a4f00ada30c33f3c9048c327b784563ef36c66
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-03-15 20:16:40 -0700

    I don't know

--- js/script.js
+++ js/script.js
@@ -4,4 +4,6 @@ const employeeExtension = document.getElementById("ext");
 const employeeEmail = document.getElementById("email");
 const employeeDepartment = document.getElementById("department");
 
-employeeID.addEventListener("click", (e) => console.log(`${e.employeeID.value}`));
+let submit = document.getElementById("empForm");
+
+submit.addEventListener("click", (e) => console.log(`${e.employeeID.value}`, `\n`, `${e.employeeName.value}`, `\n`, `${e.employeeExtension.value}`, `\n`, `${e.employeeEmail.value}`, `\n`, `${e.employeeDepartment.value}`));
