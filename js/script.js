2b94f8688bfda9b3de6ed7a400c37f89e34c1158
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-03-15 19:40:50 -0700

    Tried several ways, but just doesn't want to work

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const employeeID = document.getElementById("id");
+const employeeName = document.getElementById("name");
+const employeeExtension = document.getElementById("ext");
+const employeeEmail = document.getElementById("email");
+const employeeDepartment = document.getElementById("department");
+
+employeeID.addEventListener("keydown", () => {
+    console.log(employeeName.value)
+});
